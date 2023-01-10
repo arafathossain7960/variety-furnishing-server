78a8005d7c6471d6df836e81c8b421042d1c4355
Arafat Hossaine <arafathossain7960@gmail.comm>
2023-01-10 23:40:14 +0600

    crate booking collection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cors = require('cors');
 var jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -22,6 +22,8 @@ function run(){
     try{
         const userCollection = client.db('variety_furnishing').collection('userCollection');
         const productsCollection = client.db('variety_furnishing').collection('productsCollection');
+        const bookingCollection = client.db('variety_furnishing').collection('bookingCollection');
+        const reportCollection = client.db('variety_furnishing').collection('reportCollection');
 
 
         // jwt token validation
@@ -61,6 +63,15 @@ function run(){
             res.send(result);
         })
 
+        // get myProducts based on specific seller
+        app.get('/myProducts', async(req, res)=>{
+            const sellerEmail = req.query.sellerEmail;
+            const query = {sellerEmail: sellerEmail};
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+
         // save user information
         app.post('/user', async(req, res)=>{
 
@@ -76,6 +87,57 @@ function run(){
             const result = await productsCollection.insertOne(product);
             res.send(result);
         })
+
+
+        //booking information area------------
+      
+            // get booking
+        app.get('/bookingInfo', async(req, res)=>{
+            const userEmail= req.query.email;
+            const query = {userEmail:userEmail};
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        }) 
+            // post booking
+            app.post('/bookingInfo', async(req, res)=>{
+            const bookingInfo = req.body;
+            const result = await bookingCollection.insertOne(bookingInfo);
+            res.send(result)
+
+        })
+        // put booking
+        app.put('/bookingInfo', async(req, res)=>{
+            const id=req.query.id;
+            console.log(id)
+
+
+            const filter ={_id:ObjectId(id)};
+            const options = { upsert: true };
+            const updateBooking = {
+                $set: {
+                  payment:true 
+                }
+              };
+              const result = await bookingCollection.updateOne(filter,updateBooking,options)
+              res.send(result);
+            })
+
+            // Report to admin route
+
+            // get report
+            app.get('/report', async(req, res)=>{
+                const query ={};
+                const result =await reportCollection.find(query).toArray();
+                res.send(result);
+            })
+
+
+            // post report
+            app.post('/report',async(req, res)=>{
+                const report = req.body;
+                const result =await reportCollection.insertOne(report);
+                res.send(result)
+            })
        
     }
     finally{
